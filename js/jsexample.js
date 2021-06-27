ab7b796d924453c31f464e7446b72c4457c65c7d
elik <elik@example.com>
2021-06-27 08:53:02 +0300

    Load files

---
+++ js/jsexample.js
@@ -0,0 +1,40 @@
+﻿function AlertFunction()
+{
+    alert('Hello, World!');
+}
+
+var gameScore = 0;
+var num = 5;
+var animation = true;
+
+function HideOrShowServices()
+{
+    var el = document.getElementById("services");
+    if (el.hidden) {
+        el.hidden = false;
+    } else {
+        el.hidden = true;
+    }
+}
+
+function MoveServicesMeth()
+{
+    animation = animation == true ? false : true;
+    MoveServices();
+}
+
+function MoveServices() {
+    var el = document.getElementById("services");
+
+    if (animation) {
+        el.setAttribute("style", "position:relative;top:" + (gameScore += num).toString() + "px;");
+        if (gameScore >= el.clientHeight) {
+            num = -5;
+        } else if (gameScore <= 0) {
+            num = 5;
+        }
+        setTimeout(MoveServices, 100);
+    } else {
+        return;
+    }
+}
